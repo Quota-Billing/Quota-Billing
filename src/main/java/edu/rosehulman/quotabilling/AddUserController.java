e603edd6d7113eacce1e5419e3892e38a9b3ac72
Jack Hallam <hallamja@rose-hulman.edu>
2017-11-20 16:19:30 -0600

    testing

--- src/main/java/edu/rosehulman/quotabilling/AddUserController.java
+++ src/main/java/edu/rosehulman/quotabilling/AddUserController.java
@@ -8,23 +8,27 @@ public class AddUserController implements Route {
 
 	@Override
 	public Object handle(Request request, Response response) throws Exception {
-		String partnerId = request.params(":partnerId");
-		String productId = request.params(":productId");
-		String userId = request.params(":userId");
+		try {
+			String partnerId = request.params(":partnerId");
+			String productId = request.params(":productId");
+			String userId = request.params(":userId");
 
-		Database.getInstance().addUser(userId, productId, partnerId);
-		// TODO: Add the user to our database
+			Database.getInstance().addUser(userId, productId, partnerId);
+			// TODO: Add the user to our database
 
-		// .partnerId.TODO: Send the user to Billing
-		boolean sentToBilling = BillingClient.getInstance().addUser(partnerId, productId, userId);
+			// .partnerId.TODO: Send the user to Billing
+			boolean sentToBilling = BillingClient.getInstance().addUser(partnerId, productId, userId);
 //		if (!sentToBilling) {
 //			throw new Exception();
 //		}
-		// Add the user to our database
+			// Add the user to our database
 
-		// Send the user to Billing
+			// Send the user to Billing
 
-		// Return to Quota a success
+			// Return to Quota a success
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
 		return "";
 	}
 }
